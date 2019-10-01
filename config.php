cea08b02ff61e4854bef071141c286c84f798083
sujnan prasad <sujnan.p@impelsys.com>
2019-10-01 12:07:31 +0530

    sample code

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'sample';
+$databaseUsername = 'root';
+$databasePassword = '12345';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
